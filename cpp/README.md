96fb2bae7dba3aff4ea9a82d369359e969f02d8c
nachoaz <naxo.elderane@gmail.com>
2019-07-31 18:38:53 +0200

    Processing of C++ learning added to README.

--- cpp/README.md
+++ cpp/README.md
@@ -1,30 +1,27 @@
-# C++ Cheatsheet
+# Learning C++
 
-## Control flow
+[TOC]
 
-- Ifs in one line:
+## 1. Exercises
 
-  ```c++
-  if (a > b)
-      #sentence 1
-  else
-      #sentence 2
-  
-  # OR
-      
-  (a>b)? sentence1, sentence2
-  ```
+	1. Return max between two numbers.
+ 	2. Return min between two numbers.
+ 	3. Check if a number is within a range.
+ 	4. Compare if two strings are the same.
+ 	5. Character is contained in string.
+ 	6. Fruits array and size array.
+ 	7. Accumulated sum, loops and random.
+ 	8. ... more in process.
 
-  
+## 2. Linked List
 
-## Booleans
+This program has different exercises to practice with linked lists. Initially it was created a simple linked list. It was created as a queue to understand the syntax of the language. Once I had the simple list, more exercises were added that increased complexity. All of them have been grouped in a menu where they are tested.
 
-- `True = 0`.
-- `False != 0`. Todo lo que sea distinto de 0 es `True`
+<img src="./doc/img/index_menu.png" width="80%" height="60%">
 
+## -. In process:
 
+- Sorting algorithms.
 
-## Arrays
+- OpenCV examples.
 
-- `std::array;` C++ array
-- `tipo nombre[tamaño]`; C array
--- cpp/linked_list/main.cpp
+++ cpp/linked_list/main.cpp
@@ -13,20 +13,38 @@ using namespace std;
 //  \____/_|___/\__\__,_|  \___|_| |_|_|\__,_/___\__,_|\__,_|\__,_|
 //
 
-int program_menu(List *my_list)
+int program_menu(List *my_list, string lan)
 {
-    cout << "==== SELECCIONE UNA OPCIÓN ===============================\n"
-         << endl;
-    cout << "\t1: Insertar al principio." << endl;
-    cout << "\t2: Insertar al final." << endl;
-    cout << "\t3: Eliminar elemento del principio." << endl;
-    cout << "\t4: Eliminar elemento del final." << endl;
-    cout << "\t5: Insertar en posición." << endl;
-    cout << "\t6: Eliminar en posición." << endl;
-    cout << "\t7: Imprimir la lista" << endl;
-    cout << "\t8: Introducir elementos aleatorios en la lista." << endl;
-    cout << "\t0: Salir" << endl;
-    cout << "\n==========================================================" << endl;
+    if (lan == "es"){
+        cout << "==== SELECCIONE UNA OPCIÓN ===============================\n"
+            << endl;
+        cout << "\t1: Insertar al principio." << endl;
+        cout << "\t2: Insertar al final." << endl;
+        cout << "\t3: Eliminar elemento del principio." << endl;
+        cout << "\t4: Eliminar elemento del final." << endl;
+        cout << "\t5: Insertar en posición." << endl;
+        cout << "\t6: Eliminar en posición." << endl;
+        cout << "\t7: Imprimir la lista." << endl;
+        cout << "\t8: Introducir elementos aleatorios en la lista." << endl;
+        cout << "\t9: Cambiar de idioma." << endl;
+        cout << "\t0: Salir." << endl;
+        cout << "\n==========================================================" << endl;
+    } else if (lan == "en") {
+        cout << "==== CHOOSE AN OPTION ====================================\n" << endl;
+        cout << "\t1: Insert at the beginning." << endl;
+        cout << "\t2: Insert at the end." << endl;
+        cout << "\t3: Delete element at the beginning." << endl;
+        cout << "\t4: Delete element at the end." << endl;
+        cout << "\t5: Insert in position" << endl;
+        cout << "\t6: Delete in position." << endl;
+        cout << "\t7: Print List." << endl;
+        cout << "\t8: Insert random numbers in the list." << endl;
+        cout << "\t9: Change language." << endl;
+        cout << "\t0: Exit." << endl;
+        cout << "\n==========================================================" << endl;
+    } else{
+        cout << "Invalid Language" << endl;
+    }
 
     int option;
     int valor;
@@ -35,14 +53,29 @@ int program_menu(List *my_list)
 
     switch (option) {
         case 1:
-            cout << "Inserte un valor: ";
+            if (lan == "es"){
+                cout << "Inserte un valor: ";
+            } else if (lan == "en") {
+                cout << "Insert Value: ";
+            } else {
+                cout << "Invalid language" << endl;
+            }
             cin >> valor;
 
             my_list->insertBegin(valor);
             return option;
 
         case 2:
-            cout << "Inserte un valor: ";
+            if (lan == "es")
+            {
+                cout << "Inserte un valor: ";
+            }
+            else if (lan == "en")
+            {
+                cout << "Insert Value: ";
+            } else {
+                cout << "Invalid language" << endl;
+            }
             cin >> valor;
 
             my_list->insertEnd(valor);
@@ -69,7 +102,17 @@ int program_menu(List *my_list)
             return option;
 
         case 8:
-            cout << "\n--> Introducido un conjunto de números aleatorios\n\n" << endl;
+            if (lan == "es")
+            {
+                cout << "\n--> Introducido un conjunto de números aleatorios\n\n" << endl;
+                
+            }
+            else if (lan == "en")
+            {
+                cout << "\n--> Random values inserted\n\n" << endl;
+            } else {
+                cout << "Invalid language" << endl;
+            }
             
             /* Crear una lista aleatoria */
             for (int i = 0; i < 5; i++) {
@@ -77,6 +120,40 @@ int program_menu(List *my_list)
 
                 my_list->insertEnd(rnd_number);
             }
+            return option;
+
+        case 9:
+            if (lan == "es")
+            {
+                cout << "\n--> Elige un idioma (es o en): \n\n" << endl;
+
+                cin >> lan;
+
+                cout << "  _     _       _            _   _     _     _   " << endl;
+                cout << " | |   (_)_ __ | | _____  __| | | |   (_)___| |_ " << endl;
+                cout << " | |   | | '_ \\| |/ / _ \\/ _` | | |   | / __| __|" << endl;
+                cout << " | |___| | | | |   <  __/ (_| | | |___| \\__ \\ |_ " << endl;
+                cout << " |_____|_|_| |_|_|\\_\\___|\\__,_| |_____|_|___/\\__|" << endl;
+                cout << "" << endl;
+            }
+            else if (lan == "en")
+            {
+                cout << "\n--> Choose a language(es or en): \n\n" << endl;
+
+                cin >> lan;
+
+                cout << "   __ _     _                     _                    _" << endl;
+                cout << "  / /(_)___| |_ __ _    ___ _ __ | | __ _ ______ _  __| | __ _" << endl;
+                cout << " / / | / __| __/ _` |  / _ \\ '_ \\| |/ _` |_  / _` |/ _` |/ _` |" << endl;
+                cout << "/ /__| \\__ \\ || (_| | |  __/ | | | | (_| |/ / (_| | (_| | (_| |" << endl;
+                cout << "\\____/_|___/\\__\\__,_|  \\___|_| |_|_|\\__,_/___\\__,_|\\__,_|\\__,_|" << endl;
+                cout << "---------------------------------------------------------------" << endl;
+                cout << "" << endl;
+                
+            } else {
+                cout << "Invalid language" << endl;
+            }
+
             return option;
 
         case 0:
@@ -90,13 +167,14 @@ int program_menu(List *my_list)
 
 main() /* Main program */
 {
-    cout << "   __ _     _                     _                    _" << endl;
-    cout << "  / /(_)___| |_ __ _    ___ _ __ | | __ _ ______ _  __| | __ _" << endl;
-    cout << " / / | / __| __/ _` |  / _ \\ '_ \\| |/ _` |_  / _` |/ _` |/ _` |" << endl;
-    cout << "/ /__| \\__ \\ || (_| | |  __/ | | | | (_| |/ / (_| | (_| | (_| |" << endl;
-    cout << "\\____/_|___/\\__\\__,_|  \\___|_| |_|_|\\__,_/___\\__,_|\\__,_|\\__,_|" << endl;
-    cout << "---------------------------------------------------------------" << endl;
+    cout << "  _     _       _            _   _     _     _   " << endl;
+    cout << " | |   (_)_ __ | | _____  __| | | |   (_)___| |_ " << endl;
+    cout << " | |   | | '_ \\| |/ / _ \\/ _` | | |   | / __| __|" << endl;
+    cout << " | |___| | | | |   <  __/ (_| | | |___| \\__ \\ |_ " << endl;
+    cout << " |_____|_|_| |_|_|\\_\\___|\\__,_| |_____|_|___/\\__|" << endl;
     cout << "" << endl;
+
+    string lan = "en";
 
     /* Se crea la lista */
     List* my_list = new List();
@@ -105,7 +183,7 @@ main() /* Main program */
     int option = 0;
     do
     {
-        option = program_menu(my_list);
+        option = program_menu(my_list, lan);
         if (option != 0) {
             my_list->print_simple_list();
         }
