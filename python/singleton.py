1c6ee999dd9220b44174b0cb2356f0c22fe67867
NachoAz <n.arranz.agueda@gmail.com>
2020-04-06 21:42:15 +0200

    Singleton demo

---
+++ python/singleton.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Singleton
+
+class SoyUnico(object):
+
+    class __SoyUnico:
+        def __init__(self):
+            self.nombre = None
+
+        def __str__(self):
+            return 'self' + ' ' + self.nombre
+
+    instance = None
+
+    def __new__(cls):
+        if not SoyUnico.instance:
+            SoyUnico.instance = SoyUnico.__SoyUnico()
+        return SoyUnico.instance
+
+    def __getattr__(self, nombre):
+        return getattr(self.instance, nombre)
+
+    def __setattr__(self, nombre, valor):
+        return setattr(self.instance, nombre, valor)
+
+
+ricardo = SoyUnico()
+ricardo.nombre = "Ricardo Moya"
+print(ricardo)
+ramon = SoyUnico()
+ramon.nombre = "Ramón Invarato"
+print(ramon)
+
+print(ricardo)
+print(ramon)
+
+
+class SoyUnico(object):
+
+    __instance = None
+    nombre = None
+
+    def __str__(self):
+        return 'self' + ' ' + self.nombre
+
+    def __new__(cls):
+        if SoyUnico.__instance is None:
+            SoyUnico.__instance = object.__new__(cls)
+        return SoyUnico.__instance
+
+ricardo = SoyUnico()
+ricardo.nombre = "Ricardo Moya"
+print(ricardo)
+ramon = SoyUnico()
+ramon.nombre = "Ramón Invarato"
+print(ramon)
+
+print(ricardo)
+print(ramon)
+
