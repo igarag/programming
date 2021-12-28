9b1e04be78f8954b38544f8b0838b2d49aa55afb
igarag <n.arranz.agueda@gmail.com>
2021-12-28 20:30:09 +0100

    :sparkles: Add exercise 01, part B

--- python/aoc-2021/src/01.py
+++ python/aoc-2021/src/01.py
@@ -2,25 +2,40 @@
 Exercise 1: Sonar Sweep
 """
 
-def compute_depth(data: list) -> None:
+def solve_hard(data: list) -> None:
     counter: int = 0
-    for idx, measure in enumerate(data):
-        if idx == 0 or idx == len(data):
-            prev = measure
-            continue
-        if measure > prev:
+    for idx in range(3, len(data)):
+        a = data[idx-1] + data[idx-2] + data[idx-3]
+        b = data[idx] + data[idx-1] + data[idx-2]
+        if b > a:
             counter += 1
-        prev = measure
-    print(f"Total increased measures: {counter}")
+    print(f"Part B: Total Measures (3-measure sliding window): {counter}")
+
+
+def solve(data: list) -> None:
+    counter: int = 0
+    for idx in range(1, len(data)):
+        if data[idx] > data[idx-1]:
+            counter += 1
+    print(f"Part A: Total measures: {counter}")
+
+
+def read_data(file_path: str) -> list:
+    data = list()
+    with open(file_path) as file:
+        for item in file:
+            code = int(item.replace("\n", ""))
+            data.append(code)
+    return data
+
 
 def main():
 
-    data = list()
-    with open("01.txt") as file:
-        for item in file:
-            data.append(int(item.strip("\n")))
+    file_name = "01.txt"
+    data = read_data(file_name)
 
-    compute_depth(data)
+    solve(data)
+    solve_hard(data)
 
 
 if __name__ == "__main__":
